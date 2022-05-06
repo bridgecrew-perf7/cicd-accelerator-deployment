6d07cc0d54e6d3b52c25a5947231af11df411ead
Ponnusamy <surendharselvakumar@gmail.com>
2022-05-07 01:34:38 +0530

    Changes added for backend completion

--- cicd-accelerator-backend/db/db_connection.js
+++
@@ -1,9 +0,0 @@
-var mysql      = require('mysql');
-
-
-var connection = mysql.createConnection({
-  host     : '192.168.1.2',
-  user     : 'root',
-  password : 'admin@123',
-  database : 'cicd_accelerator'
-});
